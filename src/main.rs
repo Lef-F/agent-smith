ca4344043d90a1526ed08eb4fb94bf2f21aad7bb
lef.filippakis <Lef-F@users.noreply.github.com>
2023-04-28 00:28:20 +0200

    fix: key-val parameters cannot contain other special characters than _

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create a new ChatGPT executor
     let exec = executor!()?;
     // Create our step containing our prompt template
-    // TODO: replace kubectl with {{cli-tool}}
-    // TODO: replace {{text}} with {{user-request}}
-    let step = Step::for_prompt(prompt!(
-        "You are a specialist agent in the kubectl commandline tool.
+    let step = Step::for_prompt_template(prompt!(
+        "You are a specialist agent in the {{cli_tool}} commandline tool.
         You are asked to solve an issue that the user is facing.
-        You will respond with a brief paragraph explaining the solution.
-        In the last line of your output you will provide the full command that the user shall execute.",
-        "{{text}}"
+        Your response has to be only the following YAML template:
+
+        ```yaml
+        explanation: Your brief explanation goes here.
+        command: {{cli_tool}} shell command solution goes here.
+        ```
+
+        Only replace the information as required.
+        Do not include any text outside of the template.",
+        "{{user_request}}"
     ));
 
-    // TODO: figure out why the multi-parameter setup did not work
-    // let params = parameters!("cli-tool" => "kubectl", "user-request" => "I need to list all the namespaces.");
-    let params = parameters!("I need to list all the namespaces.");
-    println!("{}", step.prompt());
+    let params = parameters!(
+        "cli_tool" => "kubectl",
+        "user_request" => "I need to list all the namespaces."
+    );
+    println!("{}", step.format(&params).unwrap());
 
     let res = step.run(&params, &exec).await?;
+    println!("{:?}", res);
     println!("{}", res);
 
     Ok(())
